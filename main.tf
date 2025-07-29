856759007fb350ad0a180873183837b14106e52b
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2025-07-29 18:13:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "btp_subaccount" "project" {
-  name      = "sample project1"
-  subdomain = "sampleproject1"
+  name      = "sample project2"
+  subdomain = "sampleproject2"
   region    = lower(var.region)
 }
 
 resource "btp_subaccount_environment_instance" "cloudfoundry" {
   subaccount_id    = btp_subaccount.project.id
   name             = "sample-cf-instance"
+  landscape_label  = "cf-eu10"
   environment_type = "cloudfoundry"
   service_name     = "cloudfoundry"
   plan_name        = "standard"
