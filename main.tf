2ad03824e2d06546c592e51c4c91c9cebca30b03
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2025-08-04 12:14:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ region = lower(var.region)
 resource "btp_subaccount" "project" {
   name      = "sample project2"
   subdomain = "sampleproject2"
-  region    = locals.region
+  region    = local.region
 }
 
 resource "btp_subaccount_environment_instance" "cloudfoundry" {
   subaccount_id    = btp_subaccount.project.id
   name             = "sample-cf-instance"
-  landscape_label  = "cf-${locals.region}"
+  landscape_label  = "cf-${local.region}"
   environment_type = "cloudfoundry"
   service_name     = "cloudfoundry"
   plan_name        = "standard"
